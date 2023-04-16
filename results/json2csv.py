0b2a80d70a0f31ba5fcc530897449cd68df0dc9f
German Yakimov <alcatraz@fctools.com>
2023-04-16 11:23:26 +0700

    add csv results

--- results/json2csv.py
+++ results/json2csv.py
@@ -6,7 +6,7 @@ print(os.listdir('.'))
 
 files = os.listdir('.')
 for file in files:
-    if file != 'json2csv.py':
+    if file.endswith('json'):
         with open(file, 'r', encoding='utf-8') as f:
             data = json.load(f)
         data_csv = []
@@ -14,7 +14,7 @@ for file in files:
         for row in data:
             row_dict = {'authors': ','.join(row['authors']), 'title': row['title'],
                         'pub_date': row['publication_date'], 'source': row['source'],
-                        'doi': row['doi'], 'abstract': row['abstract'], 'rank': row['rank']}
+                        'doi': row['doi'], 'abstract': row['abstract'], 'rank': row['rank'], 'url': '\n'.join(row['urls'])}
             data_csv.append(row_dict)
 
         with open(file.replace('json', 'csv'), 'w', encoding='utf-8') as f:
