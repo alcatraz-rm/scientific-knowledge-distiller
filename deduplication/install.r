98b61c8e16fc2214866c6e2f04e26d8a3e6d4d62
German Yakimov <alcatraz@fctools.com>
2023-05-13 12:00:43 +0700

    fix installing dedup module

--- deduplication/install.r
+++ deduplication/install.r
@@ -6,7 +6,7 @@ install.packages("snakecase", repos="http://cran.us.r-project.org")
 # remove.packages("ASySD")
 
 # package on github is broken
-devtools::install_github("camaradesuk/ASySD")
+# devtools::install_github("camaradesuk/ASySD")
 
 # install local fixed ASySD
 devtools::install_local("ASySD")
